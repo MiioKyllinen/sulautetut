576d6e4e1409217e92bea8fc9511db2996085810
MiioKyllinen <69619453+MiioKyllinen@users.noreply.github.com>
2020-09-01 11:30:32 +0300

    Sääasema juttuja
    Sääasema progress

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,10 +1,46 @@
 import React from 'react'
+import Victory, { VictoryChart, VictoryBar, VictoryAxis } from 'victory'
 
 function Weather() {
     return (
         <React.Fragment>
-            <h1>Weather</h1>
-            <p>This is the Weather app</p>
+            <VictoryChart
+            domainPadding={{x: 45, t:10}}
+                width={500}
+                height={300}>
+            <VictoryBar
+                data={[
+
+                    {experiment: '1.1.', actual: 100},
+                    {experiment: '1.2.', actual: 5},
+                    {experiment: '1.3.', actual: 1}
+
+
+                ]} 
+                style={{data:
+                        { stroke : "green", strokeWidth: 5}
+                        
+                }}
+                x='experiment'                
+                y='actual'
+                />  
+                <VictoryAxis
+              label="experiment"
+                style={{
+        axisLabel: { padding: 30 }
+      }}
+    />
+    <VictoryAxis dependentAxis
+      label="Kosteuden %"
+      style={{
+        axisLabel: { padding: 40 }
+      }}
+    />
+        
+
+
+            </VictoryChart>
+
         </React.Fragment>
     )
 }
