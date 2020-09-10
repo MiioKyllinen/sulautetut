6894b5112926b57f6c7bf0e04ef628ac06e8a178
MiioKyllinen <69619453+MiioKyllinen@users.noreply.github.com>
2020-09-10 10:38:23 +0300

    Sääasema 10.9

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -5,56 +5,110 @@ import ReactDOM from 'react-dom'
 function Weather() {
 
   const days = [
-    { x: "1.1" },
-    { x: "2.1" },
-    { x: "3.1" },
-    { x: "4.1" },
-    { x: "5.1" },
-    { x: "6.1" },
+    { x: new Date(2020, 1, 1) },
+    { x: new Date(2020, 2, 1) },
+    { x: new Date(2020, 3, 1) },
+    { x: new Date(2020, 4, 1) },
+    { x: new Date(2020, 5, 1) },
+    { x: new Date(2020, 6, 1) },
+    { x: new Date(2020, 7, 1) },
+    { x: new Date(2020, 8, 1) },
+    { x: new Date(2020, 9, 1) },
+    { x: new Date(2020, 10, 1) }
   ]
 
   const Temperature = [
-    { x: new Date(1, 1), y: 10 },
-    { x: new Date(2, 1), y: -6 },
-    { x: new Date(3, 1), y: 13 },
-    { x: new Date(4, 1), y: 6 },
-    { x: new Date(5, 1), y: 10 },
-    { x: new Date(6, 1), y: -10 },
-    { x: new Date(7, 1), y: 3 },
-    { x: new Date(8, 1), y: 10 },
-    { x: new Date(9, 1), y: 20 },
+    { x: new Date(2020, 1, 1), y: 10 },
+    { x: new Date(2020, 2, 1), y: -6 },
+    { x: new Date(2020, 3, 1), y: 13 },
+    { x: new Date(2020, 4, 1), y: 6 },
+    { x: new Date(2020, 5, 1), y: 10 },
+    { x: new Date(2020, 6, 1), y: -10 },
+    { x: new Date(2020, 7, 1), y: 3 },
+    { x: new Date(2020, 8, 1), y: 10 },
+    { x: new Date(2020, 9, 1), y: 20 },
   ]
 
   const Humidity = [
-    { x: new Date(1, 1), y: 10 },
-    { x: new Date(2, 1), y: 6 },
-    { x: new Date(3, 1), y: 13 },
-    { x: new Date(4, 1), y: 5 },
-    { x: new Date(5, 1), y: 15 },
-    { x: new Date(6, 1), y: 45 },
-    { x: new Date(7, 1), y: 35 },
-    { x: new Date(8, 1), y: 20 },
-    { x: new Date(9, 1), y: 80 },
+    { x: new Date(2020, 1, 1), y: 10 },
+    { x: new Date(2020, 2, 1), y: 6 },
+    { x: new Date(2020, 3, 1), y: 13 },
+    { x: new Date(2020, 4, 1), y: 5 },
+    { x: new Date(2020, 5, 1), y: 15 },
+    { x: new Date(2020, 6, 1), y: 45 },
+    { x: new Date(2020, 7, 1), y: 35 },
+    { x: new Date(2020, 8, 1), y: 20 },
+    { x: new Date(2020, 9, 1), y: 80 },
 
-  
+
   ]
 
 
   return (
+
+
 
     <VictoryChart
       theme={VictoryTheme.material}
       height={500} width={1000}
       domainPadding={{ x: 30, y: 5 }}
     >
-    {/* <VictoryAxis
+
+      <VictoryAxis
         scale="time"
         standalone={false}
-        style={}
         tickValues={days}
-        tickFormat={}
-     /> */}
+        tickFormat={
+          (x) => {
+            return 'Day'
+            /* if (x.getFullYear() === 2000) {
+              return x.getFullYear();
+            }
+            if (x.getFullYear() % 5 === 0) {
+              return x.getFullYear().toString().slice(2);
+            } */
+          }
+        }
 
+      />
+
+      < VictoryAxis dependentAxis
+        domain={[-20, 30]}
+        offsetX={50}
+        orientation="left"
+        standalone={false}
+        style={{ fill: "#458ca8" }
+        }
+      />
+
+      <VictoryLine
+        style={{
+          data: { stroke: "#f01616" },
+          parent: { border: "1px solid #ccc" }
+        }}
+        domain={{
+          x: [new Date(2020, 1, 1), new Date(2020, 10, 1)],
+          y: [-20, 30]
+        }}
+        scale={{ x: "time", y: "linear" }}
+        standalone={false}
+        data={Temperature}
+        interpolation="monotoneX"
+      />
+      <VictoryLine // Keskilinja kummallekkin data linjalle
+        data={[
+          { x: new Date(2020, 1, 1), y: 0 },
+          { x: new Date(2020, 10, 1), y: 0 }
+        ]}
+        domain={{
+          x: [new Date(2020, 1, 1), new Date(2020, 10, 1)],
+          y: [-20, 30]
+        }}
+        scale={{ x: "time", y: "linear" }}
+        standalone={false}
+        style={{ data: { fill: "#999999" } }}
+
+      />
 
       <VictoryAxis dependentAxis //oikea data puolisko
         domain={[0, 100]}
@@ -68,49 +122,14 @@ function Weather() {
         style={{ data: { fill: "#458ca8" } }}
         data={Humidity}
         domain={{
-          x: [new Date(1, 1), new Date(10, 1)],
+          x: [new Date(2020, 1, 1), new Date(2020, 10, 1)],
           y: [0, 100]
-        }}
-        scale={{x: "time", y: "linear"}}
-        standalone={false}
-      />
-
-      <VictoryAxis dependentAxis
-        domain={[-20, 30]}
-        offsetX={50}
-        orientation="left"
-        standalone={false}
-        style={{ fill: "#458ca8" }}
-      />
-
-      <VictoryLine
-        style={{
-          data: { stroke: "#f01616" },
-          parent: { border: "1px solid #ccc" }
-        }}
-        domain={{
-          x: [new Date(1, 1), new Date(10, 1)],
-          y: [-20, 30]
-        }}
-        scale={{x: "time", y: "linear"}}
-        standalone={false}
-        data={Temperature}
-        interpolation="monotoneX"
-      />
-      <VictoryLine // Keskilinja kummallekkin data linjalle
-        data={[
-          { x: new Date(1, 1), y: 0 },
-          { x: new Date(10, 1), y: 0 }
-        ]}
-        domain={{
-          x: [new Date(1, 1), new Date(10, 1)],
-          y: [-20, 30]
         }}
         scale={{ x: "time", y: "linear" }}
         standalone={false}
-        style={{ data: { fill: "#999999" } }}
-
       />
+
+
 
 
     </VictoryChart>
