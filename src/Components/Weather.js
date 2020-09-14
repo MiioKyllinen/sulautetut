85b636c9629264ca1fa81f15c8a6d505a71e2fbb
MiioKyllinen <69619453+MiioKyllinen@users.noreply.github.com>
2020-09-15 01:54:56 +0300

    Easy
    a

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -20,15 +20,16 @@ function Weather() {
   ]
 
   const Temperature = [
-    { x: 1.1, y: 10 },
-    { x: 2.1, y: -6 },
-    { x: 3.1, y: 13 },
-    { x: 4.1, y: 6 },
-    { x: 5.1, y: 10 },
-    { x: 6.1, y: -10 },
-    { x: 7.1, y: 3 },
-    { x: 8.1, y: 10 },
-    { x: 9.1, y: 20 },
+    { x: new Date(2020, 1, 1), y: 4 },
+    { x: new Date(2020, 1, 2), y: 10 },
+    { x: new Date(2020, 1, 3), y: 14 },
+    { x: new Date(2020, 1, 4), y: 17 },
+    { x: new Date(2020, 1, 5), y: 10 },
+    { x: new Date(2020, 1, 6), y: 15 },
+    { x: new Date(2020, 1, 7), y: 26 },
+    { x: new Date(2020, 1, 8), y: 20 },
+    { x: new Date(2020, 1, 9), y: 25 },
+    { x: new Date(2020, 1, 10), y: 20 },
   ]
 
   const Humidity = [
@@ -54,13 +55,14 @@ function Weather() {
     domainPadding={{ x: 0, y: 0 }}
   >
 
-    <VictoryAxis dependentAxis
+  {/* <VictoryAxis dependentAxis
       domain={[-20, 30]}
       offsetX={50}
       orientation="left"
       standalone={false}
       tickValues={[30, 20, 10, 0, -10, -20]}
       style={{ ticklabels: { fill: "#7fe5f0" } }} />
+  */}
 
     <VictoryLine
       style={{
@@ -68,6 +70,7 @@ function Weather() {
         parent: { border: "1px solid #ccc" }
       }}
       domain={{
+        x: [new Date(2020, 1, 1),new Date(2020, 1, 10)],
         y: [-20, 30]
       }}
       scale={{ x: "time", y: "linear" }}
@@ -75,15 +78,52 @@ function Weather() {
       data={Temperature}
       interpolation="monotoneX" />
 
+   
+
 
   </VictoryChart>
 
 
 
 
-  )
+    )
+
+  
 } 
+function Weather2 () {
+
+  const Humidity = [
+    { x: new Date(2020, 1, 1), y: 10 },
+    { x: new Date(2020, 2, 1), y: 6 },
+    { x: new Date(2020, 3, 1), y: 13 },
+    { x: new Date(2020, 4, 1), y: 5 },
+    { x: new Date(2020, 5, 1), y: 15 },
+    { x: new Date(2020, 6, 1), y: 45 },
+    { x: new Date(2020, 7, 1), y: 35 },
+    { x: new Date(2020, 8, 1), y: 20 },
+    { x: new Date(2020, 9, 1), y: 80 },
+  ]
+
+ return (
+
+     <VictoryChart
+      theme={VictoryTheme.material}
+      height={400} width={700}
+      domainPadding={{ x: 0, y: 0 }}
+      offsetX={100} />
+
+      ,
+
+      <VictoryBar
+      data={[Humidity]}
+      domain={{
+        x: [new Date(2020, 1, 1), new Date(2020, 1, 10)],
+        y: [0, 100]
+      }}
+      
+      />
+ )
+}
 
 
-
-export default Weather 
+export default (Weather,Weather2)
